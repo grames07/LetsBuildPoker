d0e7d94d090543af1d9ab628931af151c37ba080
grames07 <gradyames07@gmail.com>
2026-04-09 00:07:20 +0000

    Wed, Apr 8, 2026, 8:07 PM -04:00

--- simple_dealer.py
+++ simple_dealer.py
@@ -1,3 +1,5 @@
+import random
+
 suits = ["Hearts", "Diamonds", "Clubs", "Spades"]
 numbers = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"]
 deck = []
@@ -7,7 +9,14 @@ for suit in suits:
         card = number + " of " + suit
         deck.append(card)
 
-print(deck)
+number_of_players = 4
 
 for player in number_of_players: #need to figure out how to define number of players
-    player_hand = 
+    player_card_1 = random.choice(deck)
+    deck.remove(player_card_1)
+    player_card_2 = random.choice(deck)
+    deck.remove(player_card_2)
+    player_hand = player_card_1, player_card_2
+    print(player_hand)
+
+
