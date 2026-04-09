7751089b125e8d0cf84fd24e6e3b5bfe443d6267
grames07 <gradyames07@gmail.com>
2026-04-09 00:26:07 +0000

    Wed, Apr 8, 2026, 8:26 PM -04:00

--- pset3_client.py
+++ pset3_client.py
@@ -5,13 +5,17 @@ import random
 HOST = '127.0.0.1'  # The server's hostname or IP address
 PORT = 65444        # The port used by the server
 
-moves = ['rock','paper','scissors']
+client_bank_account = 1000
+server_bank_account = 1000
+
 
 
 
 def main(): #this function will contain a single round of poker
     print('## Welcome to POKER! ##')
 
+    with create_new_socket() as s:
+        s.connect(HOST, PORT)
 
 ###########################################################################################################
 ###########################################################################################################
@@ -52,62 +56,8 @@ def main(): #this function will contain a single round of poker
     flop_message = f"The flop: {flop}"
     s.sendall(str(flop_message))
     print(flop_message)
-    
-###################################################################################################################
-################################################################################################################
-            #CREATING A MESSAGE TO BE DISPLAYED AT SERVER
 
-            server_message = (f'You: {server_move}   Opponent: {client_move}' + '\n')
-
-            #add message to the latest round:
-            if server_move==client_move:
-                server_message += "It's a tie!"
-
-            elif (server_move == 'rock' and client_move == 'paper') or  (server_move == 'paper' and client_move == 'scissors') or (server_move == 'scissors' and client_move == 'rock'):
-                server_message += 'You lose!'
-                cwin_counter += 1
-                match_counter += 1
-
-            else:
-                server_message += 'You win!'
-                swin_counter += 1
-                match_counter += 1
-
-            #INSERT SOMETHING HERE THAT MAKES IT BREAK OR REPEAT DEPENDING ON NUMBER OF WINS
-            if cwin_counter == 3 or swin_counter == 3:
-                server_message += ('\n' + f'rounds = {match_counter}' + f'   wins = {swin_counter}' + f'   losses = {cwin_counter}' + '\n')
-                if cwin_counter > swin_counter:
-                    server_message += ("You lost the match!")
-                if cwin_counter < swin_counter:
-                    server_message += ("You won the match!")
-
-            s.sendall(str(server_message))
-
-######################################################################################################################
-            #BELOW IS THE CLIENT SIDE DISPLAY
-
-            print(f'You: {client_move}   Opponent: {server_move}')
-
-            if server_move==client_move.lower():
-                print("It's a tie!")
-
-            elif (server_move == 'rock' and client_move == 'paper') or  (server_move == 'paper' and client_move == 'scissors') or (server_move == 'scissors' and client_move == 'rock'):
-                print('You win!')
-
-
-            else:
-                print('You lose!')
-
-
-            #INSERT SOMETHING HERE THAT MAKES IT BREAK OR REPEAT DEPENDING ON NUMBER OF WINS
-            if cwin_counter == 3 or swin_counter == 3:
-                s.sendall(str())
-                print(f'rounds = {match_counter}' + f'   wins = {cwin_counter}' + f'   losses = {swin_counter}')
-                if cwin_counter > swin_counter:
-                    print("You won the match!")
-                if cwin_counter < swin_counter:
-                    print("You lost the match!")
-                break
+    client_move = input('Enter your move (fold, check, raise): ')
 
 
 
